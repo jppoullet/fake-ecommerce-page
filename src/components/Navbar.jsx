bff0a529792544d23318882e4b6dfd052d01e0a3
jppoullet <jppoullet@yahoo.com>
2024-04-17 06:50:09 -0400

    icon styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,19 @@ const Navbar = ({ setRenderedData, data }) => {
   };
 
   const categoryHandler = (event) => {
-    // setIsOpen(!isOpen);
+    console.log(event.target.id);
+    const selectedCategory = event.target.id;
+
+    const category = data.filter((d) => {
+      return d.category === selectedCategory;
+    });
+
+    setRenderedData(category);
+    console.log(category);
+  };
+
+  const mobileCategoryHandler = (event) => {
+    setIsOpen(!isOpen);
     console.log(event.target.id);
     const selectedCategory = event.target.id;
 
@@ -36,7 +48,14 @@ const Navbar = ({ setRenderedData, data }) => {
         </div>{" "}
         <div className="flex items-center gap-5">
           <button>
-            <img src={cartIcon} alt="cartIcon" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-red-300"
+              width="22"
+              height="20"
+            >
+              <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" />
+            </svg>
           </button>
           <button>
             <img src={acctImage} alt="acctImage" width={25} />
@@ -50,22 +69,22 @@ const Navbar = ({ setRenderedData, data }) => {
         }`}
       >
         <li>
-          <a onClick={categoryHandler} id="men's clothing">
+          <a onClick={mobileCategoryHandler} id="men's clothing">
             Men
           </a>
         </li>
         <li>
-          <a onClick={categoryHandler} id="women's clothing">
+          <a onClick={mobileCategoryHandler} id="women's clothing">
             Women
           </a>
         </li>
         <li>
-          <a onClick={categoryHandler} id="jewelery">
+          <a onClick={mobileCategoryHandler} id="jewelery">
             Jewelry
           </a>
         </li>
         <li>
-          <a onClick={categoryHandler} id="electronics">
+          <a onClick={mobileCategoryHandler} id="electronics">
             Electronics
           </a>
         </li>
@@ -75,7 +94,7 @@ const Navbar = ({ setRenderedData, data }) => {
       <div className="md:flex justify-between hidden fixed bg-white top-0 left-0 right-0 px-5 py-4 z-50">
         <div className="flex items-end gap-5 text-darkGrayishBlue">
           <h1 className="text-3xl font-bold mr-6 text-black cursor-pointer">
-            JPs Store
+            <a href="/">JPs Store</a>
           </h1>
           <ul className="flex gap-5">
             <li
