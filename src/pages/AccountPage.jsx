ac53637f8a086e78392e501915083b4f243f1181
jppoullet <jppoullet@yahoo.com>
2025-01-28 12:48:37 -0500

    supabase login added

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import supabase from "../config/supabaseClient";
 import Signup from "../components/Signup";
 
 const AccountPage = () => {
-  return (
-    <div className="mt-20">
-      <h1>AccountPage</h1>
-      <div>signup</div>
-      <Signup />
-    </div>
-  );
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} />;
+  } else {
+    return <div>Logged in!</div>;
+  }
+  // return (
+  //   <div className="mt-20">
+  //     <h1>AccountPage</h1>
+  //     <div>signup</div>
+  //     <Signup />
+  //   </div>
+  // );
 };
 
 export default AccountPage;
