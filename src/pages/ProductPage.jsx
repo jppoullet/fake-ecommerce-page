6236a486d2ea750906aa46d6c28b867fe7d64203
jppoullet <jppoullet@yahoo.com>
2025-03-25 12:55:39 -0400

    mobile: add to cart reposition, product title needs aligned left

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -34,17 +34,19 @@ const ProductPage = () => {
   if (!product) return <p className="mt-20">Product not found</p>;
 
   return (
-    <div className="mt-32 flex flex-col items-center gap-2 mx-auto">
-      <p>{product.title}</p>
+    <div className="mt-20 flex flex-col items-center gap-2 mx-auto">
+      <div className="flex flex-col items-start">
+        <p className="font-bold">{product.title}</p>
+        <p className="font-bold">${product.price.toFixed(2)}</p>
+      </div>
+
       <img
         src={product.image}
         alt={product.title}
         className="object-contain size-60"
       />
-      <p>${product.price.toFixed(2)}</p>
-      <p>{product.description}</p>
       <button
-        className="flex justify-center bg-orange-400 w-1/4 my-2 py-2 rounded-md gap-2 text-white drop-shadow-sm"
+        className="flex justify-center bg-orange-400 my-2 py-2 px-4 rounded-md gap-2 text-white drop-shadow-sm"
         onClick={(e) => {
           addToCartHandler(e, product);
         }}
@@ -57,8 +59,9 @@ const ProductPage = () => {
         >
           <path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" />
         </svg>
-        <p className="drop-shadow-md">Add to Cart</p>
+        Add to Cart
       </button>
+      <p className="text-sm">{product.description}</p>
     </div>
   );
 };
